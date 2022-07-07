58dfa5b5ee86fa279781f67edbe4c961e4df2cd8
KwonJinHong <108498668+KwonJinHong@users.noreply.github.com>
2022-07-07 11:58:15 +0900

    함수 - 정수 N개의 합

---
+++ 15596/src/Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        class Test {
+            long sum(int[] a) {
+                long ans = 0;
+
+                for (int i = 0; i < a.length; i++) {
+                    ans += a[i];
+                }
+                return ans;
+            }
+        }
+    }
+
+}
