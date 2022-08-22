a5d4369db86fe4f6afbd2052b001df93596ce2a8
KwonJinHong <108498668+KwonJinHong@users.noreply.github.com>
2022-08-22 10:33:17 +0900

    정수론 및 조합론 - 배수와 약수

---
+++ 백준예제/5086/src/Main.java
@@ -0,0 +1,29 @@
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException{
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        while (true) {
+            String[] s = br.readLine().split(" ");
+
+            int a = Integer.parseInt(s[0]);
+            int b = Integer.parseInt(s[1]);
+
+            if (a == 0 && b == 0) {
+                break;
+            } else if (b%a == 0) {
+                bw.write("factor" + "\n");
+            } else if (a%b == 0) {
+                bw.write("multiple" + "\n");
+            } else {
+                bw.write("neither" + "\n");
+            }
+        }
+        bw.flush();
+        bw.close();
+    }
+}
