001aa303cd6f2000f6a90d7cbea351b4cb485820
KwonJinHong <108498668+KwonJinHong@users.noreply.github.com>
2022-08-26 21:56:24 +0900

    Getting Started - Thymeleaf를 이용한 Hello World 출력

---
+++ SpringBoot/Projects/PracThymeleaf1/src/main/java/com/example/practhymeleaf1/PracThymeleaf1Application.java
@@ -0,0 +1,13 @@
+package com.example.practhymeleaf1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracThymeleaf1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PracThymeleaf1Application.class, args);
+    }
+
+}
---
+++ SpringBoot/Projects/PracThymeleaf1/src/test/java/com/example/practhymeleaf1/PracThymeleaf1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.practhymeleaf1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracThymeleaf1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
