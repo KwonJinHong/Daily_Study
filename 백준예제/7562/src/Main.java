84d86f0b0516a69dd18f43a8d0a64d344a80ac23
KwonJinHong <108498668+KwonJinHong@users.noreply.github.com>
2022-08-30 22:15:03 +0900

    그래프와 순회 - 나이트의 이동

---
+++ 백준예제/7562/src/Main.java
@@ -0,0 +1,78 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+
+    static int[][] chess;
+    static boolean[][] visited;
+    static int n;
+    static StringBuilder sb = new StringBuilder();
+
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int testCase = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < testCase; i++) {
+
+            n = Integer.parseInt(br.readLine());
+
+            chess = new int[n][n];
+            visited = new boolean[n][n];
+
+            String[] startXY = br.readLine().split(" ");
+            String[] objectXY = br.readLine().split(" ");
+
+            int x1 = Integer.parseInt(startXY[0]);
+            int y1 = Integer.parseInt(startXY[1]);
+            int x2 = Integer.parseInt(objectXY[0]);
+            int y2 = Integer.parseInt(objectXY[1]);
+
+            if (x1 == x2 && y1 == y2) {
+                sb.append(0).append("\n");
+            } else {
+                bfs(x1, y1, x2, y2);
+            }
+        }
+        System.out.println(sb);
+
+    }
+
+    public static void bfs(int x, int y, int objectX, int objectY) {
+        Queue<int[]> queue = new LinkedList<>();
+
+        queue.add(new int[] {x, y}); // 입력받은 좌표를 큐에 삽입
+        chess[x][y] = 1;
+        int[] dx = {-2, -2, -1, -1, 1, 1, 2, 2};
+        int[] dy = {-1, 1, -2, 2, -2, 2, -1, 1};
+
+
+        while (!queue.isEmpty()) {
+            int tempX = queue.peek()[0];
+            int tempY = queue.peek()[1];
+            queue.poll();
+            visited[tempX][tempY] = true;
+
+            for (int i = 0; i < 8; i++) {
+                int nx = tempX + dx[i];
+                int ny = tempY + dy[i];
+
+                if (nx == objectX && ny == objectY) {
+                    sb.append(chess[tempX][tempY]).append("\n");
+                    return;
+                }
+
+                if (nx >= 0 && nx < n && ny >= 0 && ny < n) {
+                    if (!visited[nx][ny]) {
+                        queue.add(new int[] {nx, ny});
+                        visited[nx][ny] = true;
+                        chess[nx][ny] = chess[tempX][tempY] + 1;
+                    }
+                }
+            }
+        }
+    }
+}
