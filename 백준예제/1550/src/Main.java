e1e06ab9c503138a0cb2a87b11c5b7aff9356c0e
KwonJinHong <108498668+KwonJinHong@users.noreply.github.com>
2022-12-02 16:17:44 +0900

    수학 - 16진수

---
+++ 백준예제/1550/src/Main.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        try {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+            String n = br.readLine();
+
+            System.out.println(Integer.parseInt(n, 16));
+
+        } catch (IOException e) {
+            System.out.println("error");
+        }
+    }
+}
