7cc16c30e14f1313225f7397e2de7d7d0e6cdd44
KwonJinHong <108498668+KwonJinHong@users.noreply.github.com>
2022-12-12 20:01:27 +0900

    구현 - 화성 수학

---
+++ 백준예제/5355/src/Main.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        for(int i = 0; i < T; i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            double num = Double.parseDouble(st.nextToken());
+            while(st.hasMoreTokens()){
+                String oper = st.nextToken();
+                if(oper.equals("@")){
+                    num *= 3;
+                } else if(oper.equals("%")){
+                    num += 5;
+                } else if(oper.equals("#")){
+                    num -= 7;
+                }
+            }
+            System.out.printf("%.2f\n", num);
+        }
+        br.close();
+    }
+}
